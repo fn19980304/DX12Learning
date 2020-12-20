be8cd2e62c3ab4ae6c024263ee425ff4b8e2435f
fn <496903741@qq.com>
2020-12-20 17:00:19 +0800

    添加几何图形辅助结构体

--- Application/Common/d3dUtil.h
+++ Application/Common/d3dUtil.h
@@ -88,6 +88,78 @@ public:
     int LineNumber = -1;
 };
 
+// 用SubmeshGeometry来定义MeshGeometry中存储的单个几何体
+struct SubmeshGeometry
+{
+    UINT IndexCount = 0;          // 索引数量
+    UINT StartIndexLocation = 0;  // （全局）索引缓冲区中预读取的起始索引
+    INT BaseVertexLocation = 0;   // 基准顶点地址
+
+    // 通过此子网格来定义当前SubmeshGeometry结构体中所存几何体的包围盒
+    DirectX::BoundingBox Bounds;
+};
+
+// MeshGeometry适用于将多个几何体数据存于一个顶点缓冲区和一个索引缓冲区的情况
+// 它提供了对存于顶点缓冲区和索引缓冲区中的单个几何体进行绘制所需的数据和偏移量
+struct MeshGeometry
+{
+    // 指定几何体网格集合的名称
+    std::string Name;
+
+    // 系统内存中的副本
+    // 由于顶点/索引可以是泛型格式，用Blob类型表示，待用户在使用时再将其转换为适当的类型
+    Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+    Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+    // 对应缓冲区GPU资源
+    Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+    Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+    // 对应上传缓冲区
+    Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+    Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+    // 相关数据
+    UINT VertexByteStride = 0;                       // 每个顶点元素所占用的字节数
+    UINT VertexBufferByteSize = 0;                   // 顶点缓冲区大小（字节）
+    DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;  // 索引元素格式
+    UINT IndexBufferByteSize = 0;                    // 索引缓冲区大小（字节）
+
+    // 一个MeshGeometry结构体能够存储一组顶点/索引缓冲区中的多个几何体
+    // 若利用下列容器来定义子网格几何体，我们就能单独地绘制出其中的子网格（单个几何体）
+    // 通过名字找到相应子网格几何体
+    std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
+
+    // 顶点缓冲区视图
+    D3D12_VERTEX_BUFFER_VIEW VertexBufferView()const
+    {
+        D3D12_VERTEX_BUFFER_VIEW vbv;
+        vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+        vbv.StrideInBytes = VertexByteStride;
+        vbv.SizeInBytes = VertexBufferByteSize;
+
+        return vbv;
+    }
+
+    // 索引缓冲区视图
+    D3D12_INDEX_BUFFER_VIEW IndexBufferView()const
+    {
+        D3D12_INDEX_BUFFER_VIEW ibv;
+        ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+        ibv.Format = IndexFormat;
+        ibv.SizeInBytes = IndexBufferByteSize;
+
+        return ibv;
+    }
+
+    // 待数据上传GPU后，就可以释放内存
+    void DisposeUploaders()
+    {
+        VertexBufferUploader = nullptr;
+        IndexBufferUploader = nullptr;
+    }
+};
+
 // ThrowIfFailed宏定义
 // 对于x的调用结果用hr存储，之后转变为Unicode字符串，将错误信息输出到消息框中
 #ifndef ThrowIfFailed
