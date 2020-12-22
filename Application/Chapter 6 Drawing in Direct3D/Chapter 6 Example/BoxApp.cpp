037dfe5db86bf993c1e3c3049e6a43966270277c
fn <496903741@qq.com>
2020-12-22 18:39:10 +0800

    完成根签名创建、着色器编译及输入布局描述方法

--- Application/Chapter 6 Drawing in Direct3D/Chapter 6 Example/BoxApp.cpp
+++ Application/Chapter 6 Drawing in Direct3D/Chapter 6 Example/BoxApp.cpp
@@ -52,9 +52,15 @@ private:
 
 private:
 
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
 	ComPtr<ID3D12DescriptorHeap> mCbvHeap = nullptr;
 
 	std::unique_ptr<UploadBuffer<ObjectConstants>> mObjectCB = nullptr;
+
+	ComPtr<ID3DBlob> mvsByteCode = nullptr;
+	ComPtr<ID3DBlob> mpsByteCode = nullptr;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
 }; 
 
 BoxApp::BoxApp(HINSTANCE hInstance) :D3DApp(hInstance)
@@ -114,4 +120,69 @@ void BoxApp::BuildConstantBuffers()
 	md3dDevice->CreateConstantBufferView(
 		&cbvDesc,
 		mCbvHeap->GetCPUDescriptorHandleForHeapStart());
+}
+
+// 创建根签名
+void BoxApp::BuildRootSignature()
+{
+	// 着色器程序一般需要以资源作为输入（常量缓冲区、纹理、采样器等）
+	// 根签名则定义了着色器程序所需要的具体资源
+	// 如果把着色器程序看作一个函数，而将输入的资源当作向函数传递的参数数据
+	// 那么便可类似地认为根签名定义的是函数签名
+
+	// 根参数可以是描述符表、根描述符或根常量
+	CD3DX12_ROOT_PARAMETER slotRootParameter[1];
+
+	// 创建由单个CBV所组成的描述符表
+	CD3DX12_DESCRIPTOR_RANGE cbvTable;
+	cbvTable.Init(
+		D3D12_DESCRIPTOR_RANGE_TYPE_CBV,  // 描述符表的类型 
+		1,                                // 表中描述符的数量
+		0);                               // 将这段描述符区域绑定至基址着色器寄存器
+
+	// 以描述符表初始化根参数
+	slotRootParameter[0].InitAsDescriptorTable(
+		1,           // 描述符区域数量
+		&cbvTable);  // 指向描述符区域数组的指针 
+
+	// 根签名由一组根参数构成
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(1, slotRootParameter, 0, nullptr,
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+	// 先将根签名的描述布局进行序列化处理
+	ComPtr<ID3DBlob> serializedRootSig = nullptr;
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
+		serializedRootSig.GetAddressOf(), errorBlob.GetAddressOf());
+
+	if (errorBlob != nullptr)
+	{
+		::OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+	}
+	ThrowIfFailed(hr);
+
+	// 创建仅含一个槽位（该槽位指向一个仅由单个常量缓冲区组成的描述符区域，即cbvTable）的根签名
+	ThrowIfFailed(md3dDevice->CreateRootSignature(
+		0,
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(&mRootSignature)));
+}
+
+// 编译着色器字节码和输入布局描述
+void BoxApp::BuildShadersAndInputLayout()
+{
+	HRESULT hr = S_OK;
+
+	// 顶点着色器、像素着色器
+	mvsByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "VS", "vs_5_0");
+	mpsByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "PS", "ps_5_0");
+
+	// 构成输入布局描述的数组
+	// 语义, 附加到语义上的索引, 元素格式（数据类型）, 输入槽, 偏移量, 高级技术备选项, 高级技术备选项
+	mInputLayout =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+	};
 }
